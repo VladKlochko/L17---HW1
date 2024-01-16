20fabe5036749ee32091b35c9ec37a5c95d18d5f
Vladik <vlad53830@gmail.com>
2024-01-16 15:55:31 +0200

    Added Employee.cpp and fill

--- L17 - HW1/Employee.cpp
+++ L17 - HW1/Employee.cpp
@@ -2,62 +2,85 @@
 
 Employee::Employee()
 {
+    name = "undefined";
+    birthyear = 0;
+    position = "undefined";
+    salary = 0.0;
+    education = "undefined";
 }
 
 Employee::Employee(string name, int birthyear, string position, float salary, string education)
 {
+    setName(name);
+    setBirthYear(birthyear);
+    setPosition(position);
+    setSalary(salary);
+    setEducation(education);
+
 }
 
 string Employee::getName() const
 {
-    return string();
+    return name;
 }
 
 int Employee::getBirthYear() const
 {
-    return 0;
+    return birthyear;
 }
 
 string Employee::getPosition() const
 {
-    return string();
+    return position;
 }
 
 float Employee::getSalary() const
 {
-    return 0.0f;
+    return salary;
 }
 
 string Employee::getEducation() const
 {
-    return string();
+    return education;
 }
 
 void Employee::setName(string name)
 {
+    this->name = name;
 }
 
 void Employee::setBirthYear(int birthyear)
 {
+    this->birthyear = birthyear;
 }
 
 void Employee::setPosition(string position)
 {
+    this->position = position;
 }
 
 void Employee::setSalary(float salary)
 {
+    this->salary = salary;
 }
 
 void Employee::setEducation(string education)
 {
+    this->education = education;
 }
 
 bool Employee::isOlderThan60() const
 {
-    return false;
+    int currentYear = 2024;
+    return (currentYear - birthyear) > 60;
 }
 
 void Employee::show() const
 {
+    cout << "Работник:\n"
+        << "Имя: " << name << endl
+        << "Год Рождения: " << birthyear << endl
+        << "Должность: " << position << endl
+        << "Зарплата: " << salary << endl
+        << "Образование: " << education << endl;
 }
